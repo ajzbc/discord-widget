befebe5d2e6e2daadcfdd65383ecce3084a9265f
hxcl <thehexicle@gmail.com>
2016-12-13 18:30:02 -0800

    Deleted testing crap

--- js/reload.js
+++ js/reload.js
@@ -3,5 +3,3 @@ function reload () {
     document.getElementById("box").style.color = document.getElementById("colorID").value;
     document.getElementById("line").innerHTML = "&lt;script type='text/javascript' src='http://hexicle.com/discord-widget/widget.js' link='"+document.getElementById("linkID").value+"' text='"+document.getElementById("textID").value+"' color='"+document.getElementById("colorID").value+ "'&gt;&lt;/script&gt";
 }
-
-console.log("i do nothing");
